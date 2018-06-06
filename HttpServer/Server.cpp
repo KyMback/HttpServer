464a4726682f9e86524d77a4a2177a1f6cf07e1b
KyMback <dark_AHrel@mail.ru>
2018-06-06 20:01:41 +0300

    Fixes

--- HttpServer/Server.cpp
+++ HttpServer/Server.cpp
@@ -18,15 +18,11 @@ int Server::Start()
 	this->ServerConfigurations = new Configuration();	
 	this->ServerConfigurations->OpenNewSocket();
 	cout << "Socket successfully created..." << endl;
-	int listenSocket = ServerConfigurations->GetSocketFileDescriptor();	
 	cout << "Start listening in 127.0.0.1:8000..." << endl;
 
 	while(true) {
-		int clientSocket = INVALID_SOCKET;
-		sockaddr clientAddress;
-		int address_len = sizeof(clientAddress);
-		clientSocket = accept(listenSocket, &clientAddress, &address_len);
-		//cout << "Client socket descriptor: " << clientSocket << " " << clientAddress.sa_family << " " << address_len <<  endl;
+		auto clientSocket = INVALID_SOCKET;
+		clientSocket = accept(ServerConfigurations->GetSocketFileDescriptor(), nullptr, nullptr);
 		if (clientSocket == INVALID_SOCKET) {
 			printf("accept failed");
 		}
@@ -37,7 +33,7 @@ int Server::Start()
 
 DWORD WINAPI Server::StartNewSession(LPVOID param)
 {
-	Session* session = new Session(new Connection(reinterpret_cast<int>(param)));
+	auto session = new Session(new Connection(reinterpret_cast<int>(param)));
 	int result = 0; 
 	try
 	{
